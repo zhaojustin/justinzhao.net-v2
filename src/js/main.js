ea344ec80f574d232fb4a5cee25d1c22eaf79af4
Justin Zhao <justin@justinzhao.net>
2021-07-17 15:25:17 -0700

    fixed links and images

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
-const swup = new Swup({
-    plugins: [new SwupSlideTheme()]
-  });
+const options = {
+  plugins: [new SwupSlideTheme()],
+  linkSelector:
+    'a[href^="' +
+    window.location.origin +
+    '"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup]), a[href^="../"]:not([data-no-swup])',
+};
+const swup = new Swup(options);
