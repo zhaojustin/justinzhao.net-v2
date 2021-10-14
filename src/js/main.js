0fae72e8d1779c4ac5d6dc3d72f764b3e52f9737
Justin Zhao <justin@justinzhao.net>
2021-10-13 23:19:43 -0400

    async loading project page

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,11 @@ const options = {
     '"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup]), a[href^="../"]:not([data-no-swup])',
 };
 const swup = new Swup(options);
+
+function init() {}
+
+// run once
+init();
+
+// this event runs for every page view after initial load
+swup.on("contentReplaced", init);
